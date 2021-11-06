99d2f8fc83937a7e97c82e740cb7ed1359e86f01
difrancesco1 <90358453+difrancesco1@users.noreply.github.com>
2021-11-06 17:54:38 -0400

    organize code, added health bars and updated the fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,28 +1,23 @@
-// window.alert("This is an alert! JavaScript is running!"); 
-//. alert() function tells the browser to display a message to the user.
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-// This creates a function called "fight"
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// this calls our function "fight"
-//fight();
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+  
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+  
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+  };
 
-// Asks the user thier robots name with the prompt function (user input)
-// Stores the name under the variable "playName" so we can call for it later
-// A variale is a named location
-var playerName = window.prompt("What is your robot's name?")
-
-//Will display the user inputted Robot's name
-console.log(playerName);
-
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
-
-var name = "your name";
-console.log(name);
+fight();
