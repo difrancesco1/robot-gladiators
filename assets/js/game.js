47482020a9fbd3f4c543880506f4bdfd66536bde
difrancesco1 <90358453+difrancesco1@users.noreply.github.com>
2021-11-06 18:13:21 -0400

    Created a if/else statement to see if both the robot has died AND will tell the player what hp they are at

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,6 +18,23 @@ var fight = function() {
   
     // Log a resulting message to the console so we know that it worked.
     console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } 
+
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } 
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
   };
 
 fight();
