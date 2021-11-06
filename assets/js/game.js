04cb9ddc72d54ba3ff6edfaeb441497a8d572796
difrancesco1 <90358453+difrancesco1@users.noreply.github.com>
2021-11-06 17:25:02 -0400

    Updated js code to add variables for robot name and players name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,28 @@
-window.alert("This is an alert! JavaScript is running!"); 
+// window.alert("This is an alert! JavaScript is running!"); 
+//. alert() function tells the browser to display a message to the user.
+
+// This creates a function called "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// this calls our function "fight"
+//fight();
+
+// Asks the user thier robots name with the prompt function (user input)
+// Stores the name under the variable "playName" so we can call for it later
+// A variale is a named location
+var playerName = window.prompt("What is your robot's name?")
+
+//Will display the user inputted Robot's name
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
